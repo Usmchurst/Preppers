017019782bfa78fc80a53003b5ce55ad38207eaf
noah gaston <noahsfuture18@gmail.com>
2022-06-02 09:57:22 -0400

    stash

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,58 +1,21 @@
 import { useState } from 'react'
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Login from './components/LoginForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/LoginForm';
+import Signup from './components/Signup'
+
 
 function App() {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  async function createUser(event) {
-    event.preventDefault()
-
-    const res = await fetch('http://localhost:3001/user/login',{
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        username, 
-        email, 
-        password
-      })
-    })
-
-    const data = await res.json();
-
-    console.log(data)
-  };
-
   return (
     <div>
-      <h1>signup</h1>
-      <form onSubmit={createUser}>
-        <input 
-          value={username}
-          onChange={(e)=> setUsername(e.target.value)}
-          type="text"
-          placeholder="username" 
-        /> <br/>
-       <input 
-          value={email}
-          onChange={(e)=> setEmail(e.target.value)}
-          type="email"
-          placeholder="email" 
-        /> <br/>
-        <input 
-        value={password}
-        onChange={(e)=> setPassword(e.target.value)}
-        type="password"
-        placeholder="password" 
-      /> <br/>
-      <input type='submit' value="submit"/>
-      </form>
+      <div>
+      <Login />
+      </div>
+      <div>
+      <Signup />
+      </div>
     </div>
+    
   );
 }
 
--- Server/routes/User-Routes/index.js
+++ Server/routes/User-Routes/index.js
@@ -1,14 +1,39 @@
 const { User } = require('../../models');
+const jwt = require('jsonwebtoken');
 
 const router = require('express').Router();
   
-router.post('/login', async (req, res) => {
+router.post('/signup', async (req, res) => {
     try{
-        console.log(req.body);    
-    res.json({ status: 'ok' });
+        await User.create({
+           username: req.body.username,
+           email: req.body.email,
+           password: req.body.password,
+       })
+       res.json({ status: 'ok' });
     } catch(err) {
-
+        res.status(404).send({ error: err});
     };
+});
+
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ 
+        email: req.body.email,
+         password: req.body.password,
+    })
+
+    if(user) {
+
+        const token = jwt.sign(
+        {   
+            username: user.username,
+            email: user.email,
+        }, 'secret123')
+
+        return res.json({ status: 'ok', user:token });
+    } else {
+        return res.json({ status: 'error', user:false });
+    }
 });
  
 module.exports = router
