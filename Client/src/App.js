96c79b764f54f1faa16df75b1b7e3da5b5dde551
noah gaston <noahsfuture18@gmail.com>
2022-06-03 12:24:36 -0400

    client side routing

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,91 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import PersonIcon from '@mui/icons-material/Person'
-import Login from './components/LoginForm';
-import Header from "./components/header.js";
+import Login from './components/LoginForm'; 
 import RegistrationForm from "./components/registrationForm";
+import Navbar from './components/navbar'
 
 
 function App() {
   return (
-    <div>  
-      <Header />
-      <Login />
-      <RegistrationForm />
-      <PersonIcon/>
+    <div>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/home"  element={<Navbar />}  />
+          <Route path="/login"  element={<Login />}  />
+          <Route path="/signup" element={<RegistrationForm />} />
+        </Routes> 
+      </BrowserRouter>
     </div>
     
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-// import { useState } from 'react'
-// import './App.css';
-// // import 'bootstrap/dist/css/bootstrap.min.css';
-// // import Login from './components/LoginForm';
-
-// function App() {
-//   const [username, setUsername] = useState('')
-//   const [email, setEmail] = useState('')
-//   const [password, setPassword] = useState('')
-//   const [confirmPassword, setConfirmPassword] = useState(null);
-
-//   async function createUser(event) {
-//     event.preventDefault()
-
-//     const res = await fetch('http://localhost:3001/user/login',{
-//       method: 'POST',
-//       headers: {
-//         'content-type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         username, 
-//         email, 
-//         password
-//       })
-//     })
-
-//     const data = await res.json();
-
-//     console.log(data)
-//   };
-
-//   return (
-//     <div>
-//       <h1>signup</h1>
-//       <form onSubmit={createUser}>
-//         <input 
-//           value={username}
-//           onChange={(e)=> setUsername(e.target.value)}
-//           type="text"
-//           placeholder="username" 
-//         /> <br/>
-//        <input 
-//           value={email}
-//           onChange={(e)=> setEmail(e.target.value)}
-//           type="email"
-//           placeholder="email" 
-//         /> <br/>
-//         <input 
-//         value={password}
-//         onChange={(e)=> setPassword(e.target.value)}
-//         type="password"
-//         placeholder="password" 
-//       /> <br/>
-//       <input type='submit' value="submit"/>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default App;
