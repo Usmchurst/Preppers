57d3682ca55d4de3b55ae6ad45a12923ffd52df9
Eric Hurst <ericahurst77@gmail.com>
2022-06-04 23:44:47 -0400

    signup page

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,14 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/LoginForm'; 
 import RegistrationForm from "./components/registrationForm";
 import Navbar from './components/navbar'
-
+import Home from './pages/A'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        <Route path="/home"  element={<Navbar />}  />
+        <Route path="/home"  element={<Home />}  />
           <Route path="/login"  element={<Login />}  />
           <Route path="/signup" element={<RegistrationForm />} />
         </Routes> 
---
+++ Client/src/pages/A.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../components/navbar';
+import Home from '../components/homepage/Homepage';
+
+
+export default function Homepage (){
+
+    return (
+        <div>
+            <Nav />
+            <Home/>
+        </div>
+    )
+
+
+}
